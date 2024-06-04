e3b6049ab64eb9bf949902b4babaa9ae3fea8259
Bautista del Cerro <baudelcerro1204@gmail.com>
2024-06-04 20:02:25 -0300

    games

---
+++ js/juegos.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true,
+    items: 1,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
